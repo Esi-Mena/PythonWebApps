ba5754f19cf19dcf8486b36bdf6deafc17b1feda
Esi Mena <mena79@bears.unco.edu>
2022-09-07 15:18:57 -0600

    view danny davito change

--- 03/Superhero/hero/views.py
+++ 03/Superhero/hero/views.py
@@ -36,3 +36,14 @@ class BlackWidow(TemplateView):
             'body': 'My name is Natasha Romanova',
             'image': '/static/images/black_widow.jpg'
         }
+
+class DannyDavito(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Danny Davito',
+            'body': 'The true Hero',
+            'image': '/static/images/Danny_Davito.jpg'
+        }
+
