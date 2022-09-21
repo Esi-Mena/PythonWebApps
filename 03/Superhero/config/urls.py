f91db127df4905d3106895b85f3e4783fdfa8568
Esi Mena <mena79@bears.unco.edu>
2022-09-21 16:12:58 -0600

    I made the settings equal to the one i made wich is the galler y one

--- 03/Superhero/config/urls.py
+++ 03/Superhero/config/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.urls.conf import include
-
+from hero.views import HeroListView, HeroView
 
 urlpatterns = [
     path('', include('hero.urls')),
+    path('<str:name>', HeroView.as_view()),
+    path('', HeroListView.as_view()),
 ]
