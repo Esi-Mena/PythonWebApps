a4e82faabb59807b0219d1382d4eaf4399537367
Esi Mena <mena79@bears.unco.edu>
2022-09-23 10:49:57 -0600

    project 4 changes hero 4

--- 03/Superhero/config/urls.py
+++ 03/Superhero/config/urls.py
@@ -1,9 +1,13 @@
+
 from django.urls import path
 from django.urls.conf import include
-from hero.views import HeroListView, HeroView
+from hero.views import HulkView, IronManView,BlackWidow,DannyDavito
 
 urlpatterns = [
     path('', include('hero.urls')),
-    path('<str:name>', HeroView.as_view()),
-    path('', HeroListView.as_view()),
+    path('<str:name>', HulkView.as_view()),
+    path('', IronManView.as_view()),
+    path('', BlackWidow.as_view()),
+    path('', DannyDavito.as_view()),
+    
 ]
