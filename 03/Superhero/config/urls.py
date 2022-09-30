6738a3dc89927a654b502ee7b68b3727a593e349
Esi Mena <mena79@bears.unco.edu>
2022-09-30 01:32:17 -0600

    hero to heroes

--- 03/Superhero/config/urls.py
+++ 03/Superhero/config/urls.py
@@ -1,15 +1,15 @@
 from django.views.generic import RedirectView
 from django.urls import path
 
-from hero.views import HeroDetailView, HeroListView
+from heroes.views import HeroDetailView, HeroListView
 
 
 urlpatterns = [
 
     # Home
-    path('', RedirectView.as_view(url='heroes/')),
+    path('', RedirectView.as_view(url='hero/')),
 
     # Photos
-    path('heroes/', HeroListView.as_view()),
-    path('heroes/<int:id>', HeroDetailView.as_view()),
+    path('hero/', HeroListView.as_view()),
+    path('hero/<int:id>', HeroDetailView.as_view()),
 ]
---
+++ 03/Superhero/heroes/views.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from django.views.generic import TemplateView
+
+
+def hero_list():
+    def hero_details(i, f):
+        caption = f'Caption for Hero {i}' if i == 1 else None
+        return dict(id=i, file=f, caption=caption)
+
+    heroes = Path('static/images').iterdir()
+    heroes = [hero_details(i, f) for i, f in enumerate(heroes)]
+    return heroes
+
+
+class HeroListView(TemplateView):
+    template_name = 'heroes.html'
+
+    def get_context_data(self, **kwargs):
+        return dict(heroes=hero_list())
+
+
+class HeroDetailView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        i = kwargs['id']
+        heroes = hero_list()
+        p = heroes[i]
+        return dict(hero=p)
